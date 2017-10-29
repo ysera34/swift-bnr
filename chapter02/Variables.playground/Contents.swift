5eeb03ae73502b0c43b734dc5cc8563e1593ef1d
ysera34 <ycyoon34@gmail.com>
2017-10-29 14:52:53 +0900

    chapter02

---
+++ chapter02/Variables.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//var numberOfStoplights = "Four"
+var numberOfStoplights: Int = 4 // type annotation
+// int numberOfStoplights = 4;
+numberOfStoplights += 2
+
+let numberOfStoplights2: Int = 4
+//numberOfStoplights2 += 2
+
+var population: Int
+population = 5422
+let townName: String = "Knowhere"
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights2) stoplights."
+print(townDescription)
