e363ef825a0dda5f4454b6b9ef6289a16d2a66f6
ysera34 <ycyoon34@gmail.com>
2017-10-29 14:21:08 +0900

    chapter01

---
+++ chapter01/MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+// String str = "Hello, playground";
+str += "!"
+// str += "!";
+
+print(str)
+//System.out.print(str);
