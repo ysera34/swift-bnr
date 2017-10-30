a9efda9f8a0cce085311e5b6c3fbbad359137ddd
ysera34 <ycyoon34@gmail.com>
2017-10-31 01:33:16 +0900

    chapter04 integer

---
+++ chapter04/Numbers.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+print("The maximum Int value is \(Int.max).")
+print("The minimum Int value is \(Int.min).")
+print("The maximum value for a 8-bit integer is \(Int8.max).")
+print("The minimum value for a 8-bit integer is \(Int8.min).")
+print("The maximum value for a 16-bit integer is \(Int16.max).")
+print("The minimum value for a 16-bit integer is \(Int16.min).")
+print("The maximum value for a 32-bit integer is \(Int32.max).")
+print("The minimum value for a 32-bit integer is \(Int32.min).")
+print("The maximum value for a 64-bit integer is \(Int64.max).")
+print("The minimum value for a 64-bit integer is \(Int64.min).")
+
+print("The maximum Int value is \(UInt.max).")
+print("The minimum Int value is \(UInt.min).")
+print("The maximum value for a 8-bit unsigned integer is \(UInt8.max).")
+print("The minimum value for a 8-bit unsigned integer is \(UInt8.min).")
+print("The maximum value for a 16-bit unsigned integer is \(UInt16.max).")
+print("The minimum value for a 16-bit unsigned integer is \(UInt16.min).")
+print("The maximum value for a 32-bit unsigned integer is \(UInt32.max).")
+print("The minimum value for a 32-bit unsigned integer is \(UInt32.min).")
+print("The maximum value for a 64-bit unsigned integer is \(UInt64.max).")
+print("The minimum value for a 64-bit unsigned integer is \(UInt64.min).")
+
