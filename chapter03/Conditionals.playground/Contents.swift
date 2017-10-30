32463ddc95cfc68c277df3a85bdb013f9eaff919
ysera34 <ycyoon34@gmail.com>
2017-10-30 23:37:43 +0900

    chapter03 if/else

---
+++ chapter03/Conditionals.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var population: Int = 5422
+var message: String
+
+if population < 10000 {
+    message = "\(population) is a small town!!"
+} else {
+    message = "\(population) is pretty big!"
+}
+print(message)
