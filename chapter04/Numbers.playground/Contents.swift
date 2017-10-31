b03ad15e02f4299f3e711a99dd7065ed177b016b
ysera34 <ycyoon34@gmail.com>
2017-10-31 09:03:42 +0900

    chapter04 integer operation

--- chapter04/Numbers.playground/Contents.swift
+++ chapter04/Numbers.playground/Contents.swift
@@ -26,3 +26,49 @@ print("The minimum value for a 32-bit unsigned integer is \(UInt32.min).")
 print("The maximum value for a 64-bit unsigned integer is \(UInt64.max).")
 print("The minimum value for a 64-bit unsigned integer is \(UInt64.min).")
 
+/*
+The maximum Int value is 9223372036854775807.
+The minimum Int value is -9223372036854775808.
+The maximum value for a 8-bit integer is 127.
+The minimum value for a 8-bit integer is -128.
+The maximum value for a 16-bit integer is 32767.
+The minimum value for a 16-bit integer is -32768.
+The maximum value for a 32-bit integer is 2147483647.
+The minimum value for a 32-bit integer is -2147483648.
+The maximum value for a 64-bit integer is 9223372036854775807.
+The minimum value for a 64-bit integer is -9223372036854775808.
+The maximum Int value is 18446744073709551615.
+The minimum Int value is 0.
+The maximum value for a 8-bit unsigned integer is 255.
+The minimum value for a 8-bit unsigned integer is 0.
+The maximum value for a 16-bit unsigned integer is 65535.
+The minimum value for a 16-bit unsigned integer is 0.
+The maximum value for a 32-bit unsigned integer is 4294967295.
+The minimum value for a 32-bit unsigned integer is 0.
+The maximum value for a 64-bit unsigned integer is 18446744073709551615.
+The minimum value for a 64-bit unsigned integer is 0.
+*/
+
+let numberOfPeople: UInt = 40
+let volumeAdjustment: Int32 = -1000
+
+print(10 + 20)
+print(30 - 5)
+print(5 * 6)
+
+print(10 + 2 * 5)
+print(30 - 5 - 5)
+
+print((10 + 2) * 5)
+print(30 - (5 - 5))
+
+print(11 / 3)
+print(11 % 3)
+print(-11 % 3)
+
+var x = 10
+x += 10  // x = x + 10
+print("x has had 10 added to it and is now \(x)")
+x -= 5   // x = x - 5
+print("x has had 5 substracted from it and is now \(x)");
+
