254d71ed537e7fbdc0dde4ec9ce03eba9fa68244
ysera34 <ycyoon34@gmail.com>
2017-11-01 08:57:42 +0900

    chapter05 switch fallthrough

--- chapter05/Switch.playground/Contents.swift
+++ chapter05/Switch.playground/Contents.swift
@@ -4,16 +4,23 @@ import Cocoa
 
 var statusCode: Int = 404;
 
-var errorString: String
+//var errorString: String
+var errorString: String = "The request failed."
 switch statusCode {
-case 400:
-    errorString = "Bad request"
-case 401:
-    errorString = "Unauthorized"
-case 403:
-    errorString = "Forbidden"
-case 404:
-    errorString = "Not found"
+//case 400:
+//    errorString = "Bad request"
+//case 401:
+//    errorString = "Unauthorized"
+//case 403:
+//    errorString = "Forbidden"
+//case 404:
+//    errorString = "Not found"
+//default:
+//    errorString = "None"
+case 400, 401, 403, 404:
+    errorString = "There was something wrong with the request."
+    fallthrough
 default:
-    errorString = "None"
+    errorString += "Please review the request and try again."
 }
+print(errorString)
