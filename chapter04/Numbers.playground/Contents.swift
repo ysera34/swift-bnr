c9832163a565e2546dd0a15da6a5eeec8ed35c92
ysera34 <ycyoon34@gmail.com>
2017-10-31 09:50:19 +0900

    chapter04 floating point number

--- chapter04/Numbers.playground/Contents.swift
+++ chapter04/Numbers.playground/Contents.swift
@@ -52,6 +52,7 @@ The minimum value for a 64-bit unsigned integer is 0.
 let numberOfPeople: UInt = 40
 let volumeAdjustment: Int32 = -1000
 
+
 print(10 + 20)
 print(30 - 5)
 print(5 * 6)
@@ -62,9 +63,11 @@ print(30 - 5 - 5)
 print((10 + 2) * 5)
 print(30 - (5 - 5))
 
+
 print(11 / 3)
 print(11 % 3)
 print(-11 % 3)
+
 
 var x = 10
 x += 10  // x = x + 10
@@ -72,3 +75,28 @@ print("x has had 10 added to it and is now \(x)")
 x -= 5   // x = x - 5
 print("x has had 5 substracted from it and is now \(x)");
 
+
+let y: Int8 = 120
+let z = y &+ 10
+print("120 &+ 10 is \(z)");
+
+
+let a: Int16 = 200
+let b: Int8 = 50
+let c = a + Int16(b)
+
+
+let d1 = 1.1
+let d2: Double = 1.1
+let f1: Float = 100.3
+
+print(10.0 + 11.4)
+print(11.0 / 3.0)
+
+if d1 == d2 {
+    print("d1 and d2 are the same!")
+}
+print("d1 + 0.1 is \(d1 + 0.1)")
+if d1 + 0.1 == 1.2 {
+    print("d1 + 0.1 is equal to 1.2")
+}
