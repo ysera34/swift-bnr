90afd09bd3667ae01e07cfdeb25700ba4702944e
ysera34 <ycyoon34@gmail.com>
2017-11-01 09:13:53 +0900

    chapter05 switch range matching

--- chapter05/Switch.playground/Contents.swift
+++ chapter05/Switch.playground/Contents.swift
@@ -5,7 +5,7 @@ import Cocoa
 var statusCode: Int = 404;
 
 //var errorString: String
-var errorString: String = "The request failed."
+var errorString: String = "The request failed with the error."
 switch statusCode {
 //case 400:
 //    errorString = "Bad request"
@@ -17,10 +17,28 @@ switch statusCode {
 //    errorString = "Not found"
 //default:
 //    errorString = "None"
-case 400, 401, 403, 404:
-    errorString = "There was something wrong with the request."
-    fallthrough
-default:
-    errorString += "Please review the request and try again."
+//case 400, 401, 403, 404:
+//    errorString = "There was something wrong with the request."
+//    fallthrough
+//default:
+//    errorString += "Please review the request and try again."
+    
+    case 101, 101:
+        errorString += " Informational, 1xx."
+    
+    case 204:
+        errorString += " Successful but no content, 204."
+    
+    case 300...307:
+        errorString += " Redirection, 3xx."
+    
+    case 400...417:
+        errorString += " Client error, 4xx"
+    
+    case 500...505:
+        errorString += " Server error, 5xx"
+    
+    default:
+        errorString += "Unknown. Please review the request and try again."
 }
 print(errorString)
