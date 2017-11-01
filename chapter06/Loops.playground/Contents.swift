fe6cca7f17630c835fdbc88f1785bb52145f75ef
ysera34 <ycyoon34@gmail.com>
2017-11-02 00:02:14 +0900

    chapter06 loop for-in loop

---
+++ chapter06/Loops.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var myFirstInt: Int = 0
+
+for i in 1...5 {
+//for _ in 1...5 {
+    myFirstInt += 1
+    myFirstInt
+//    print(myFirstInt)
+    print("myFirstInt equals \(myFirstInt) at iteration \(i)")
+}
