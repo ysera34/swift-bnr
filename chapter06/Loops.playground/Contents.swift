fe63587552950c1fd51d95ee28e8bebc1b115307
ysera34 <ycyoon34@gmail.com>
2017-11-02 12:31:21 +0900

    chapter06 loop while continue break

--- chapter06/Loops.playground/Contents.swift
+++ chapter06/Loops.playground/Contents.swift
@@ -42,3 +42,35 @@ repeat {
     print("Fire blasters!")
 } while shields > 0
 */
+
+
+var shields = 5
+var blastersOverheating = false
+var blasterFireCount = 0
+var spaceDemonsDestroyed = 0
+while shields > 0 {
+    
+    if spaceDemonsDestroyed == 5 {
+        print("You beat the game!")
+        break
+    }
+    
+    if blastersOverheating {
+        print("Blasters are overheated! Cooldown initiated.")
+        sleep(5)
+        print("Blasters ready to fire")
+        sleep(1)
+        blastersOverheating = false
+        blasterFireCount = 0
+    }
+    
+    if blasterFireCount > 5 {
+        blastersOverheating = true
+        continue
+    }
+    // fire!!
+    print("Fire blasters! blasterFireCount : \(blasterFireCount)")
+    
+    blasterFireCount += 1
+    spaceDemonsDestroyed += 1
+}
