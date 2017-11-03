50ebe0cf1e752f02a1d9665c9942980fbdc1d69d
ysera34 <ycyoon34@gmail.com>
2017-11-03 12:48:55 +0900

    chapter07 string unicode scalar

--- chapter07/Strings.playground/Contents.swift
+++ chapter07/Strings.playground/Contents.swift
@@ -9,3 +9,13 @@ mutablePlayground += "!"
 for c: Character in mutablePlayground.characters {
     print("\(c)")
 }
+
+let oneCoolDude = "\u{1F618}"
+let aAcute = "\u{0061}\u{0301}"
+for scalar in playground.unicodeScalars {
+    print("\(scalar.value)")
+}
+
+// 100 on the console is an integer-32bit
+// the integer 100 is 64 hexadecimal
+print("\u{0064}")
