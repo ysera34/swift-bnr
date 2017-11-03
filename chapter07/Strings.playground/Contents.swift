329aebc56adf8f8d8eb7ce3f9da60528ce61e5d3
ysera34 <ycyoon34@gmail.com>
2017-11-03 14:13:56 +0900

    chapter07 string canonical equivalence, count, index

--- chapter07/Strings.playground/Contents.swift
+++ chapter07/Strings.playground/Contents.swift
@@ -19,3 +19,16 @@ for scalar in playground.unicodeScalars {
 // 100 on the console is an integer-32bit
 // the integer 100 is 64 hexadecimal
 print("\u{0064}")
+
+let aAcutePrecomposed = "\u{00E1}"
+let b = (aAcute == aAcutePrecomposed)
+print ("aAcute: \(aAcute.characters.count); aAcutePrecomposed: \(aAcutePrecomposed.characters.count)")
+
+//let index = playground[3]
+let start = playground.startIndex
+let end = playground.index(start, offsetBy: 4)
+let fifthCharacter = playground[end]
+let range = start...end
+let firstFive = playground[range]
+
+
