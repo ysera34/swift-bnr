02a7c43f78499521a536297ac433b897950dd7f0
ysera34 <ycyoon34@gmail.com>
2017-11-03 12:01:43 +0900

    chapter07 string

---
+++ chapter07/Strings.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let playground = "Hello, playground"
+var mutablePlayground = "Hello, mutable playground"
+mutablePlayground += "!"
+
+for c: Character in mutablePlayground.characters {
+    print("\(c)")
+}
