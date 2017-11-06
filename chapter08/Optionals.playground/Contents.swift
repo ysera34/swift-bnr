e7c326d7fc9886341b7e88ed7c0b7fa66df651a0
ysera34 <ycyoon34@gmail.com>
2017-11-06 23:44:40 +0900

    chapter08 optional

---
+++ chapter08/Optionals.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var errorCodeString: String?
+errorCodeString = "404"
+// print("errorString : \(errorCodeString)")
+if errorCodeString != nil {
+    let theError = errorCodeString!
+    print("theError : \(theError)")
+}
+
