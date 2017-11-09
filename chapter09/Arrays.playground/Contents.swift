4da45e0bd3d2c16d259943e54ffdec5a524d13cf
ysera34 <ycyoon34@gmail.com>
2017-11-10 01:39:11 +0900

    chapter09 array

---
+++ chapter09/Arrays.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var bucketList0: Array<String>
+var bucketList1: [String] = ["Climb Mt. Everest"]
+var bucketList2 = ["Climb Mt. Everest"] // type inference
+
