ae25df586d7e2f3af5367a055b02ea25978f7823
ysera34 <ycyoon34@gmail.com>
2017-11-11 18:20:56 +0900

    chapter10 dictionary

---
+++ chapter10/Dictionaries.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var dict1: Dictionary<String, Double> = [:]
+var dict2 = Dictionary<String, Double>()
+var dict3: [String:Double] = [:]
+var dict4 = [String:Double]()
+
+var movieRatings = [
+    "Donnie Darko": 4,
+    "Chungking Express": 5,
+    "Dark City": 4
+]
