478257779df7e6881eed7a357fc9367b1397b768
ysera34 <ycyoon34@gmail.com>
2017-11-11 19:07:47 +0900

    chapter10 dictionary add, update, remove

--- chapter10/Dictionaries.playground/Contents.swift
+++ chapter10/Dictionaries.playground/Contents.swift
@@ -12,3 +12,24 @@ var movieRatings = [
     "Chungking Express": 5,
     "Dark City": 4
 ]
+
+// get value
+print("I have rated \(movieRatings.count) movies")
+let donnieRating = movieRatings["Donnie"]        // nil
+let darkoRating = movieRatings["Donnie Darko"]   // 4
+let cityRating: Int? = movieRatings["Dark City"] // 4
+
+// update value
+movieRatings["Dark City"] = 5
+let oldRating: Int? = movieRatings.updateValue(5, forKey: "Donnie Darko")
+if let lastRating = oldRating, let currentRating = movieRatings["Donnie Darko"] {
+    print("old rating: \(lastRating); current rating: \(currentRating)")
+}
+
+// add value
+movieRatings["The Cabinet of Dr. Caligari"] = 5
+
+// remove value
+//movieRatings.removeValue(forKey: "Dark City")
+//let removedRating: Int? = movieRatings.removeValue(forKey: "Dark City")
+movieRatings["Dark City"] = nil // cannot return optional
