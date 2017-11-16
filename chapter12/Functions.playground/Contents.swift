8f4b958df78ef1d4613b1866f032a3a3b15cb111
ysera34 <ycyoon34@gmail.com>
2017-11-17 00:00:09 +0900

    chapter12 function parameter external, variadic, default, inout

--- chapter12/Functions.playground/Contents.swift
+++ chapter12/Functions.playground/Contents.swift
@@ -28,3 +28,44 @@ func printPersonalGreeting1(to: String) {
     print("Hello \(name), welcome to your playground")
 }
 printPersonalGreeting(to: "Matt")
+
+// variadic arguments
+func printPersonalGreetings(to names: String...) {
+    for name in names {
+        print("Hello \(name), welcome to your playground")
+    }
+}
+printPersonalGreetings(to: "Alex", "Chris", "Drew", "Pat")
+
+// parameter default value
+func divisionDescriptionFor1(numerator: Double,
+                             denonimator: Double,
+                             withPunctuation punctuation: String = ".") {
+    print("\(numerator) divided by \(denonimator) equals \(numerator / denonimator)\(punctuation)")
+}
+divisionDescriptionFor1(numerator: 9.0, denonimator: 3.0)
+divisionDescriptionFor1(numerator: 9.0, denonimator: 3.0, withPunctuation: "!")
+
+// in-out parameter
+var error = "The request failed"
+func appendErrorCode(_ code: Int, toErrorString errorString: inout String) {
+    if code == 400 {
+        errorString += " bad request."
+    }
+}
+appendErrorCode(400, toErrorString: &error)
+error
+// left side of mutating operator isn't mutable: 'errorString' is a 'let' constant
+//func appendErrorCode(_ code: Int, toErrorString errorString: String) {
+//    if code == 400 {
+//        errorString += " bad request."
+//    }
+//}
+
+// return value
+func divisionDescriptionFor2(numerator: Double,
+                             denonimator: Double,
+                             withPunctuation punctuation: String = ".") -> String {
+    return"\(numerator) divided by \(denonimator) equals \(numerator / denonimator)\(punctuation)"
+}
+print(divisionDescriptionFor2(numerator: 9.0, denonimator: 3.0, withPunctuation: "!"))
