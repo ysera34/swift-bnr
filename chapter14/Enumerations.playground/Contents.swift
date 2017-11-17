2898a0061eb042bbee3ba1f890178baad2a15c82
ysera34 <ycyoon34@gmail.com>
2017-11-18 01:19:08 +0900

    chapter14 enum raw value

--- chapter14/Enumerations.playground/Contents.swift
+++ chapter14/Enumerations.playground/Contents.swift
@@ -23,3 +23,49 @@ case .right:
 case .center:
     print("center aligned")
 }
+
+
+// raw value
+enum TextAlignment1: Int {
+    case left
+    case right
+    case center
+}
+var alignment2 = TextAlignment1.left
+print("Left has raw value \(TextAlignment1.left.rawValue)")
+print("Right has raw value \(TextAlignment1.right.rawValue)")
+print("Center has raw value \(TextAlignment1.center.rawValue)")
+
+enum TextAlignment2: Int {
+    case left = 10
+    case right = 20
+    case center
+}
+var alignment3 = TextAlignment1.left
+print("Left has raw value \(TextAlignment2.left.rawValue)")
+print("Right has raw value \(TextAlignment2.right.rawValue)")
+print("Center has raw value \(TextAlignment2.center.rawValue)")
+print("Left has hash value \(TextAlignment2.left.hashValue)")
+print("Right has hash value \(TextAlignment2.right.hashValue)")
+print("Center has hash value \(TextAlignment2.center.hashValue)")
+print("The alignment variable has raw value \(alignment3.rawValue)")
+
+let myRawValue = 20
+if let myAlignment = TextAlignment2(rawValue: myRawValue) {
+    print("successfully converted \(myRawValue) into a TextAlignment")
+} else {
+    print("\(myRawValue) has no corresponding TextAlignment case")
+}
+
+enum ProgrammingLanguage: String {
+    case swift //= "swift programming"
+    case objectiveC //= "objective-c programming"
+    case c = "c programming"
+    case cpp = "c++ programming"
+    case java = "java programming"
+}
+
+let myFavoriteLanguage = ProgrammingLanguage.swift
+print("My favorite programming language is \(myFavoriteLanguage.rawValue)")
+let myFavoriteLanguage1 = ProgrammingLanguage.java
+print("My favorite programming language is \(myFavoriteLanguage1.rawValue)")
