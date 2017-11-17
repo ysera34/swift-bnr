c033d57dd6de15a4c36f23d9bed08fc6762f6d4e
ysera34 <ycyoon34@gmail.com>
2017-11-18 07:19:33 +0900

    chapter14 enum method

--- chapter14/Enumerations.playground/Contents.swift
+++ chapter14/Enumerations.playground/Contents.swift
@@ -69,3 +69,36 @@ let myFavoriteLanguage = ProgrammingLanguage.swift
 print("My favorite programming language is \(myFavoriteLanguage.rawValue)")
 let myFavoriteLanguage1 = ProgrammingLanguage.java
 print("My favorite programming language is \(myFavoriteLanguage1.rawValue)")
+
+
+// method
+enum Lightbulb {
+    case on
+    case off
+    
+    func surfaceTemperature(forAmbientTemperature ambient: Double) -> Double {
+        switch self {
+        case .on:
+            return ambient + 150.0
+        case .off:
+            return ambient
+        }
+    }
+    
+    mutating func toggle() {
+        switch self {
+        case .on:
+            self = .off
+        case .off:
+            self = .on
+        }
+    }
+}
+
+var bulb = Lightbulb.on
+let ambientTemperature = 77.0
+var bulbTemperature = bulb.surfaceTemperature(forAmbientTemperature: ambientTemperature)
+print("the bulb's temperature is \(bulbTemperature)")
+bulb.toggle()
+bulbTemperature = bulb.surfaceTemperature(forAmbientTemperature: ambientTemperature)
+print("the bulb's temperature is \(bulbTemperature)")
