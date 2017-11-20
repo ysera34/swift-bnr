08a8283eb4f9fc9dd8f01caf8690db3410abcebe
ysera34 <ycyoon34@gmail.com>
2017-11-21 02:06:01 +0900

    chapter15 structure, class

---
+++ chapter15/MonsterTown/MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Yoon on 2017. 11. 21..
+//  Copyright © 2017년 Yoon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
