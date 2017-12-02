2349cf2ee1503128f88e107a64cdf0e13bd1a398
ysera34 <ycyoon34@gmail.com>
2017-12-02 14:33:10 +0900

    chapter18 cow step2

--- chapter18/IntArray.playground/Contents.swift
+++ chapter18/IntArray.playground/Contents.swift
@@ -27,15 +27,25 @@ struct IntArray {
         print(buffer.storage)
     }
     
-    func insert(_ value: Int, at index: Int) {
+    private mutating func copyIfNeeded() {
+        if !isKnownUniquelyReferenced(&buffer) {
+            print("Making a copy of \(buffer.storage)")
+            buffer = IntArrayBuffer(buffer: buffer)
+        }
+    }
+    
+    mutating func insert(_ value: Int, at index: Int) {
+        copyIfNeeded()
         buffer.storage.insert(value, at: index)
     }
     
-    func append(_ value: Int) {
+    mutating func append(_ value: Int) {
+        copyIfNeeded()
         buffer.storage.append(value)
     }
     
-    func remove(at index: Int) {
+    mutating func remove(at index: Int) {
+        copyIfNeeded()
         buffer.storage.remove(at: index)
     }
 }
@@ -45,7 +55,9 @@ integers.append(1)
 integers.append(2)
 integers.append(4)
 integers.describe()
+print("copying integers to ints")
 var ints = integers
+print("inserting into ints")
 ints.insert(3, at: 2)
 integers.describe()
 ints.describe()
