b398c96f118b014f39cadcfd35845153efb19539
ysera34 <ycyoon34@gmail.com>
2017-12-02 15:43:43 +0900

    chapter19 conform protocol

--- chapter19/Protocols.playground/Contents.swift
+++ chapter19/Protocols.playground/Contents.swift
@@ -14,9 +14,6 @@ func printTable(_ data: [[String]], withColumnLables columnLabels: String...) {
     
     for row in data {
         var out = "|"
-//        for item in row {
-//            out += " \(item) |"
-//        }
         for (j, item) in row.enumerated() {
             let paddingNeeded = columnWidths[j] - item.characters.count
             let padding = repeatElement(" ", count: paddingNeeded).joined(separator: "")
@@ -40,7 +37,15 @@ struct Person {
     let yearsOfExperience: Int
 }
 
-struct Department {
+protocol TabularDataSource {
+    var numberOfRows: Int { get }
+    var numberOfColumns: Int { get }
+    
+    func label(forColumn column: Int) -> String
+    func itemFor(row: Int, column: Int) -> String
+}
+
+struct Department: TabularDataSource {
     let name: String
     var people = [Person]()
     
@@ -51,9 +56,64 @@ struct Department {
     mutating func add(_ person: Person) {
         people.append(person)
     }
+    
+    var numberOfRows: Int {
+        return people.count
+    }
+    
+    var numberOfColumns: Int {
+        return 3
+    }
+    
+    func label(forColumn column: Int) -> String {
+        switch column {
+        case 0: return "Employee Name"
+        case 1: return "Age"
+        case 2: return "Years of Experience"
+        default: fatalError("Invalid column!")
+        }
+    }
+    
+    func itemFor(row: Int, column: Int) -> String {
+        let person = people[row]
+        switch column {
+        case 0: return person.name
+        case 1: return String(person.age)
+        case 2: return String(person.yearsOfExperience)
+        default: fatalError("Invalid column!")
+        }
+    }
+}
+
+func printTable(_ dataSource: TabularDataSource) {
+    var firstRow = "|"
+    var columnWidths = [Int]()
+//    for columnLabel in columnLabels {
+    for i in 0..<dataSource.numberOfColumns {
+        let columnLabel = dataSource.label(forColumn: i)
+        let columnHeader = " \(columnLabel) |"
+        firstRow += columnHeader
+        columnWidths.append(columnLabel.characters.count)
+    }
+    print(firstRow)
+    
+//    for row in data {
+    for i in 0..<dataSource.numberOfRows {
+        var out = "|"
+        
+//        for (j, item) in row.enumerated() {
+        for j in 0..<dataSource.numberOfColumns {
+            let item = dataSource.itemFor(row: i, column: j)
+            let paddingNeeded = columnWidths[j] - item.characters.count
+            let padding = repeatElement(" ", count: paddingNeeded).joined(separator: "")
+            out += " \(padding)\(item) |"
+        }
+        print(out)
+    }
 }
 
 var department = Department(name: "Engineering")
 department.add(Person(name: "Joe", age: 30, yearsOfExperience: 6))
 department.add(Person(name: "Karan", age: 40, yearsOfExperience: 18))
 department.add(Person(name: "Fred", age: 50, yearsOfExperience: 20))
+printTable(department)
