e22444a51adf7bb2d01eb0dd0424e8003cd45691
ysera34 <ycyoon34@gmail.com>
2017-12-09 09:27:49 +0900

    chapter21 extension custom type

--- chapter21/Extensions.playground/Contents.swift
+++ chapter21/Extensions.playground/Contents.swift
@@ -12,3 +12,31 @@ protocol Vehicle {
     var numberOfDoors: Int { get }
     var hasFlatbed: Bool { get }
 }
+struct Car {
+    let make: String
+    let model: String
+    let year: Int
+    let color: String
+    let nickname: String
+    var gasLevel: Double {
+        willSet {
+            precondition(newValue <= 1.0 && newValue >= 0.0, "New value must be between 0 and 1.")
+        }
+    }
+}
+extension Car: Vehicle {
+    var topSpeed: Velocity { return 180 }
+    var numberOfDoors: Int { return 4 }
+    var hasFlatbed: Bool { return false }
+}
+extension Car {
+    init(make: String, model: String, year: Int) {
+        self.init(make: make,
+                  model: model,
+                  year: year,
+                  color: "Black",
+                  nickname: "N/A",
+                  gasLevel: 1.0)
+    }
+}
+var car = Car(make: "Ford", model: "Fusion", year: 2013)
