1a5aef7efd62943d56cc29b3f13facd965e2d746
ysera34 <ycyoon34@gmail.com>
2017-12-16 13:11:16 +0900

    chapter25 Equatable and Comparable conform comparable protocol

--- chapter25/Comparison.playground/Contents.swift
+++ chapter25/Comparison.playground/Contents.swift
@@ -13,9 +13,25 @@ extension Point: Equatable {
     }
 }
 
+extension Point: Comparable {
+    static func <(lhs: Point, rhs: Point) -> Bool {
+        return (lhs.x < rhs.x) && (lhs.y < rhs.y)
+    }
+}
+
 let a = Point(x: 3, y: 4)
 let b = Point(x: 3, y: 4)
 
 // Before conforming Equatable Protocol, Binary operator '==' cannot be applied to two 'Point' operands
 let abEqual = (a == b)
 let abNotEqual = (a != b)
+
+let c = Point(x: 2, y: 6)
+let d = Point(x: 3, y: 7)
+
+let cdEqual = (c == d)
+let cLessThanD = (c < d)
+
+let cLessThanEqualD = (c <= d)
+let cGreaterThanD = (c > d)
+let cGreaterThanEqualD = (c >= d)
