fea16da70c23d8f17c4c09e75d90eb0388c42be0
ysera34 <ycyoon34@gmail.com>
2017-12-17 14:13:36 +0900

    chapter25 Equatable and Comparable task

--- chapter25/Comparison.playground/Contents.swift
+++ chapter25/Comparison.playground/Contents.swift
@@ -15,7 +15,12 @@ extension Point: Equatable {
 
 extension Point: Comparable {
     static func <(lhs: Point, rhs: Point) -> Bool {
-        return (lhs.x < rhs.x) && (lhs.y < rhs.y)
+//        return (lhs.x < rhs.x) && (lhs.y < rhs.y)
+        return sqrt(Double((lhs.x * lhs.x) + (lhs.y * lhs.y))) < sqrt(Double(rhs.x * rhs.x + rhs.y * rhs.y))
+    }
+    
+    static func +(lhs: Point, rhs: Point) -> Point {
+        return Point(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
     }
 }
 
@@ -35,3 +40,5 @@ let cLessThanD = (c < d)
 let cLessThanEqualD = (c <= d)
 let cGreaterThanD = (c > d)
 let cGreaterThanEqualD = (c >= d)
+
+let cAddD = (c + d)
